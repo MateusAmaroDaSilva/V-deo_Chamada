c2c74e9c2a503884fb8dd2850633975f75405b82
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2024-09-05 20:31:23 -0300

    atualizacao02

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const APP_ID = "5ed970bcc795499882f5e66ae422df80";
-const TOKEN = "007eJxTYHDzLikUTdcq4FbVUpsWVP5DdfqTwtkbb76f3TDn/BVe8YcKDKapKZbmBknJyeaWpiaWlhYWRmmmqWZmiakmRkYpaRYGVvdupDUEMjIsbPjEwAiFID4LQ05+aQoDAwACOR+g";
-const CHANNEL = "loud";
+const APP_ID = "d291ead6adcc4b4891447c361347f199";
+const TOKEN = "007eJxTYEjnvuhcNnudzREfgz/FzrL/JWW2C6yyn7RLmr978f6Z0VsUGFKMLA1TE1PMElOSk02STCwsDU1MzJONzQyNTczTDC0tFxjeSmsIZGS41VnHysgAgSA+C0NBYmYeAwMA+PIepQ==";
+const CHANNEL = "pain";
 
 const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 
 let localTracks = [];
 let remoteUsers = {};
+let screenTrack = null;
 let isJoined = false;
 
 let joinAndDisplayLocalStream = async () => {
@@ -27,15 +28,14 @@ let joinAndDisplayLocalStream = async () => {
     }
 }
 
-
 let joinStream = async () => {
     if (isJoined) return; 
     isJoined = true;
     await joinAndDisplayLocalStream();
     document.getElementById('join-btn').style.display = 'none';
-    document.getElementById('stream-controls').style.display = 'flex';
+    document.getElementById('stream-wrapper').style.display = 'flex';
+    document.getElementById('stream-controls-wrapper').style.display = 'flex';
 }
-
 
 let handleUserJoined = async (user, mediaType) => {
     remoteUsers[user.uid] = user;
@@ -76,10 +76,10 @@ let leaveAndRemoveLocalStream = async () => {
 
     await client.leave();
     document.getElementById('join-btn').style.display = 'block';
-    document.getElementById('stream-controls').style.display = 'none';
+    document.getElementById('stream-wrapper').style.display = 'none';
+    document.getElementById('stream-controls-wrapper').style.display = 'none';
     document.getElementById('video-streams').innerHTML = '';
 }
-
 
 let toggleMic = async (e) => {
     if (localTracks[0].muted) {
@@ -93,7 +93,6 @@ let toggleMic = async (e) => {
     }
 }
 
-
 let toggleCamera = async (e) => {
     if (localTracks[1].muted) {
         await localTracks[1].setMuted(false);
@@ -106,9 +105,35 @@ let toggleCamera = async (e) => {
     }
 }
 
+let toggleScreenShare = async (e) => {
+    if (!screenTrack) {
+        try {
+            screenTrack = await AgoraRTC.createScreenVideoTrack();
+            const player = `<div class="video-container" id="user-container-screen">
+                                <div class="video-player" id="user-screen"></div>
+                            </div>`;
+            document.getElementById('video-streams').insertAdjacentHTML('beforeend', player);
+            screenTrack.play('user-screen');
+            await client.publish(screenTrack);
+            e.target.innerText = 'Parar de Compartilhar';
+            e.target.style.backgroundColor = '#EE4B2B';
+        } catch (error) {
+            console.error('Erro ao compartilhar a tela: ', error);
+        }
+    } else {
+        await client.unpublish(screenTrack);
+        screenTrack.close();
+        document.getElementById('user-container-screen').remove();
+        screenTrack = null;
+        e.target.innerText = 'Compartilhar Tela';
+        e.target.style.backgroundColor = 'cadetblue';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('join-btn').addEventListener('click', joinStream);
     document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream);
     document.getElementById('mic-btn').addEventListener('click', toggleMic);
     document.getElementById('camera-btn').addEventListener('click', toggleCamera);
+    document.getElementById('share-screen-btn').addEventListener('click', toggleScreenShare);
 });
