f411baa9a2926fde3b8d9bd19f11aa62060bbe42
MateusAmaroDaSilva <ericaeandre44@gmaill.com>
2024-09-12 15:23:14 -0300

    Chat Node.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
 });
 
 // Inicia o servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
